b69825ef3b2d3ccb2f1c8f45be7d1bb7c421775f
ShivdeepChaudhari23 <shivdeepchaudhari23@gmail.com>
2024-10-31 18:19:27 +0530

    Added db port

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,8 @@ const con = await mysql.createConnection({
     host: process.env.HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 });
 
 con.connect((error) => {
